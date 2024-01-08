89d88d3e95c9e19bbadb392ea8c9cc44db461e75
D△NIEL TRI▽INO <74508444+danieltrivino1@users.noreply.github.com>
2024-01-08 09:08:19 -1000

    Create 26_remove_duplicates_from_sorted_array.py

---
+++ 26_remove_duplicates_from_sorted_array.py
@@ -0,0 +1,29 @@
+# 26. Remove Duplicates from Sorted Array
+# Solution 1 (two pointers):
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        left = 0
+        right = 0
+        k = 1
+        while right < len(nums):
+            if nums[left] == nums[right]:
+                right += 1
+            else:
+                left += 1
+                nums[left] = nums[right]
+                right += 1
+                k += 1
+        return k
+# Solution 2 (one pointer):
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        left = 1
+        k = 1
+        while left < len(nums):
+            if nums[left] == nums[left - 1]:
+                left += 1
+            else:
+                nums[k] = nums[left]
+                left += 1
+                k += 1
+        return k
